23ac99b6a7e86dcb380e8695729c78ecae861bad
Christian <christian.blanz@outlook.de>
2022-01-11 17:51:41 +0100

    - progress

--- apps/learning-project1/src/computer/computer.module.ts
+++ apps/learning-project1/src/computer/computer.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ComputerController } from './computer.controller';
+import { DiskService } from '../disk/disk.service';
+import { CpuService } from '../cpu/cpu.service';
 
 @Module({
+  imports: [DiskService, CpuService],
   controllers: [ComputerController]
 })
 export class ComputerModule {}
